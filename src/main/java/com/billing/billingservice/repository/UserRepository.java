2a53d7829237fc23378b0510f013d1f3c637c0a1
Dzmitry Mishchanka <dzmitry.mishchanka@gmail.com>
2023-05-26 03:32:29 +0200

    Add new repository query

--- src/main/java/com/billing/billingservice/repository/UserRepository.java
+++ src/main/java/com/billing/billingservice/repository/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findOneByLogin(String login);
 
+    Optional<User> findOneByEmailIgnoreCase(String email);
+
 }
